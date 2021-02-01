18b59128773e9f284b7696ccf53dabc9891d4516
Dimitri Aguilar <daguilar@emich.edu>
2021-02-01 17:05:55 -0500

    Modified adminCreation for demo purposes

--- client/pages/adminCreation.php
+++ client/pages/adminCreation.php
@@ -1,3 +1,16 @@
 <?php
 	/*Insert Code here*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Page Title</title>
+	</head>
+<body>
+
+	<!-- Insert Code here -->
+	<h1>This is a header!</h1>
+
+</body>
+</html>
