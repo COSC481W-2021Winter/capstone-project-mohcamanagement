fa8069232910adbbbbad5ac3707435343aab6944
JKuiper7 <jkuiper@emich.edu>
2021-02-21 23:31:28 -0500

    Fixing nav bar buttons

--- src/pages/userMain.php
+++ src/pages/userMain.php
@@ -1,124 +1,3 @@
-<<<<<<< HEAD:client/pages/userMain.php
-<?php
-	/*db connection needed if in seperate file */
-	include_once ('../includes/dbConnection.php');
-	// $conn is the conection to database
-<<<<<<< HEAD
-=======
-
-	// checking to see if the cookie array is empty
-	if(isset($_COOKIE["Username"])) {
-		// if not empty then we store the cookie into a variable
-		$userCookie = $_COOKIE["Username"];
-	}
->>>>>>> 01a8e8de2a947cc06424435501adf5d42d846463
-?>
-
-<html>
- <head>
-	<title>User main page</title>
-	<link rel="stylesheet" type="text/css" href="../style/style.css">
- </head>
-	<style>
-	h1{text-align: center;}
-	
-	.centerHorz {
-		display: flex;
-		justify-content: center;
-	}
-	
-	.vertical-center {
-		margin: 0;
-		position: absolute;
-		top: 50%;
-		-ms-transform: translateY(-50%);
-		transform: translateY(-50%);
-	}
-
-	.square1 {
-		text-align:center;
-		margin-left: auto;
-		margin-right: auto;
-		height: 500px;
-		width: 750px;
-		background-color: #555;
-		border: 3px solid black;
-	}
-	.square2 {
-		text-align:center;
-		margin-left: auto;
-		margin-right: auto;
-		height: 50px;
-		width: 300px;
-		background-color: #555;
-		margin-top: 10px;
-		border: 3px solid black;
-	}
-	.button {
-		border: none;
-		color: white;
-		padding: 16px 32px;
-		text-align: center;
-		text-decoration: none;
-		display: inline-block;
-		font-size: 17px;
-		margin: 10px 5px;
-		transition-duration: 0.4s;
-		cursor: pointer;
-	}
-	.button1 {
-		background-color: white; 
-		color: black; 
-		border: 2px solid #800000;
-	}
-	.button2  {
-		background-color: white; 
-		color: black; 
-		border: 2px solid #800000;
-		position: absolute;
-		left: 250px;
-		top: 100px
-	}
-	.button1:hover {
-		background-color: #671D00;
-		color: black;
-	}
-	</style>
- <header>
-		<img class="logo" src="" alt="logo">
-		<nav>
-			<ul class="nav_links">
-				<NavButton class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userUpdateAvailabilty.php' ">Update Availability</NavButton>
-				<NavButton class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userRequestOff.php' ">Request Off</NavButton>
-			</ul>
-		</nav>
-		<button0 class="button button0" onclick="location.href='/capstone-project-mohcamanagement/client/index.html'">Log Out</button0>
-	</header>
-	<body>
-<<<<<<< HEAD
-		<h1>Hello Username</h1>
-=======
-		<?php 
-		echo "<h1>Welcome $userCookie</h1>";
-		 ?>
->>>>>>> 01a8e8de2a947cc06424435501adf5d42d846463
-		<div class="square1">Schedule Placeholder</div>
-		<div class="square2">Suggested Inventory / Writeoffs Placeholder</div>
-		<!-- <div class="centerHorz">
-			<button class="button button1">Inventory Suggestions</button>
-			<button class="button button1" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userUpdateAvailabilty.php'"> Update Availability</button>
-			<button class="button button1" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userRequestOff.php'">Request Off</button>
-<<<<<<< HEAD
-		</div>
-	</body>
-</html>
-=======
-			<button class="button button1" onclick="location.href='/capstone-project-mohcamanagement/client/index.html'">Log Out</button>
-		</div> -->
-	</body>
-</html>
->>>>>>> 01a8e8de2a947cc06424435501adf5d42d846463
-=======
 <?php
 	/*db connection needed if in seperate file */
 	include_once ('../includes/dbConnection.php');
@@ -205,11 +84,11 @@
 		<img class="logo" src="" alt="logo">
 		<nav>
 			<ul class="nav_links">
-				<NavButton class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userUpdateAvailabilty.php' ">Update Availability</NavButton>
-				<NavButton class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/client/pages/userRequestOff.php' ">Request Off</NavButton>
+				<button class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/src/pages/userUpdateAvailabilty.php' ">Update Availability</NavButton>
+				<button class="button NavButton" onclick="location.href='/capstone-project-mohcamanagement/src/pages/userRequestOff.php' ">Request Off</NavButton>
 			</ul>
 		</nav>
-		<button0 class="button button0" onclick="location.href='/capstone-project-mohcamanagement/client/index.html'">Log Out</button0>
+		<button class="button button0" onclick="location.href='/capstone-project-mohcamanagement/src/index.html'">Log Out</button0>
 	</header>
 	<body>
 		<?php 
@@ -225,4 +104,3 @@
 		</div> -->
 	</body>
 </html>
->>>>>>> 611954120f2657db079e0c4932e6d5eb7dfdfad6:src/pages/userMain.php
