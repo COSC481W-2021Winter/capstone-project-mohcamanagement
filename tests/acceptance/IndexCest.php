96861b60b4dca5fcecb5999f926a32606e31c4cb
Adham Oudeif <aoudeif5@emich.edu>
2021-02-23 22:47:05 -0500

    Created the acceptance tests for Index. Changed classes from input to buttons on login and companyRegister.php

--- tests/acceptance/IndexCest.php
+++ tests/acceptance/IndexCest.php
@@ -4,25 +4,57 @@
   Directory: C:\xampp\htdocs\capstone-project-mohcamanagement
   */
 
-class SigninCest
-{
-    public function tryToTest(AcceptanceTester $I)
-    {
-        $I->amOnPage('index.html');
 
-        $I->click('Login');
-        $I->see('Login');
-    }
-}
-
-class FirstCest
+  //check to see if simple buttons work
+  
+class ClickButtonsCest
 {
-    public function frontpageWorks(AcceptanceTester $I)
+    public function RegisterButtonWorks(AcceptanceTester $I)
     {
         $I->amOnPage('index.html');
         $I->see('Mocha');
+
+        $I->see('Register');
+        $I->click('Register');
+        $I->see('Back');
+        $I->click('Back');
+
+       $I->see('Mocha');
+
     }
 
+    public function AboutButtonWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('index.html');
+        $I->see('Mocha');
+
+        $I->see('About');
+        $I->click('About');
+        $I->see('Adham Oudeif');
+        $I->click('Go Back');
+
+       $I->see('Mocha');
+
+    }
+}
+
+//Check to see if we can navigate through pages and use them
+
+class SignInCest
+{
+    public function LoginButtonWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('index.html');
+
+        $I->see('Login');
+        $I->click('Login');
+        $I->fillField('Username', 'DKilroy');
+        $I->fillField('Pin', '1117');
+        $I->see('Submit');
+        $I->click('Submit');
+        $I->see('Welcome DKilroy');
+
+    }
 }
 
 ?>
