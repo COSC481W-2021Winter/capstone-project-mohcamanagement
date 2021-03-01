f741d7f6137963faad92fbb27fa56514fed4e721
Dimitri Aguilar <daguilar@emich.edu>
2021-02-28 21:01:21 -0500

    Defined function headers for inventoryOrderTest.php

--- tests/acceptance/inventoryOrderCest.php
+++ tests/acceptance/inventoryOrderCest.php
@@ -2,7 +2,37 @@
 
 class inventoryOrderCest
 {
+	public function inventoryOrderAcceptanceTest(AcceptanceTester $I)
+	{
+		$I->amOnPage('/pages/inventoryOrder.php');
+		$I->see('Inventory Log');
+	}
 
+	public function backButtonUserTest(AcceptanceTester $I)
+	{
+
+	}
+
+
+	public function backButtonAdminTest(AcceptanceTester $I)
+	{
+
+	}
+
+	public function amountVerificationTest(AcceptanceTester $I)
+	{
+
+	}
+
+	public function dataReceivedVerificationTest(AcceptanceTester $I)
+	{
+
+	}
+
+	public function verifyTableEntriesTest(AcceptanceTester $I)
+	{
+
+	}
 }
 
 ?>
