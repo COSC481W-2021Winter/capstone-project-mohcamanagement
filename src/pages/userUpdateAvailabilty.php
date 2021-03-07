2e10d780907a29bc3804ad20d7f45c7394ea09b2
dillonKilroy <dkilroy@emich.edu>
2021-03-06 23:17:44 -0500

    Hussain and I worked on getting the page loading the new data from the tabels. Also a user is able to update their availability from a form in the page.

--- src/pages/userUpdateAvailabilty.php
+++ src/pages/userUpdateAvailabilty.php
@@ -1,71 +1,106 @@
 <?php
-	$username=$_COOKIE['Username'];
+	$userCookie = $_COOKIE["Username"];
+	session_start();
 	include("../includes/dbConnection.php");
 
-	if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		$query = "SELECT * from Users where Username='$username'";
-		$result = mysqli_query($conn, $query);
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['Add'])) {
+		$pin = $_SESSION['Pin'];
+		if(!empty($_POST['DaySelection']) && !empty($_POST['ShiftNamesSelection'])){
+			$daySelected = $_POST['DaySelection'];
+			$shiftNameSelected = $_POST['ShiftNamesSelection'];
 
-		$currMon= '';
-		$currTue= '';
-		$currWed= '';
-		$currThur= '';
-		$currFri= '';
-		$currSat= ''; 
-		$currSun = '';
+			if($shiftNameSelected == 'Off') {
+				$query = "DELETE FROM Availability WHERE Pin = '$pin' AND Day = '$daySelected'"; 
+				mysqli_query($conn, $query);
 
-		$row = mysqli_fetch_assoc($result);
-			$currMon = $row['Monday'];
-			$currTue = $row['Tuesday'];
-			$currWed = $row['Wednesday'];
-			$currThur = $row['Thursday'];
-			$currFri = $row['Friday'];
-			$currSat = $row['Saturday'];
-			$currSun = $row['Sunday'];
+				$query = "INSERT INTO Availability VALUES ('$daySelected','$pin','$shiftNameSelected')";
+				mysqli_query($conn, $query);
+			}
+			else {
+				$query = "SELECT * FROM Availability WHERE Pin = '$pin' AND Day = '$daySelected'";
+				$result = mysqli_query($conn, $query);
+				$numOfRows = mysqli_num_rows($result);
+
+				for($i = 0; $i<$numOfRows; $i++) {
+					$row = mysqli_fetch_assoc($result);
+					$shiftName = $row['ShiftName'];
+					if($shiftName == 'Off') {
+						$query = "DELETE FROM Availability WHERE Pin = '$pin' AND Day = '$daySelected'"; 
+						mysqli_query($conn, $query);
+						break;
+					}
+				}
+
+				$query = "INSERT INTO Availability VALUES ('$daySelected','$pin','$shiftNameSelected')";
+				mysqli_query($conn, $query);
+			}
+		}
+		else {
+			echo "<script>alert('Error both selection need input.')</script>";
+		}
+	}
+	
+
+	// 	$currMon= '';
+	// 	$currTue= '';
+	// 	$currWed= '';
+	// 	$currThur= '';
+	// 	$currFri= '';
+	// 	$currSat= ''; 
+	// 	$currSun = '';
+
+	// 	$row = mysqli_fetch_assoc($result);
+	// 		// $currMon = $row['Monday'];
+	// 		// $currTue = $row['Tuesday'];
+	// 		// $currWed = $row['Wednesday'];
+	// 		// $currThur = $row['Thursday'];
+	// 		// $currFri = $row['Friday'];
+	// 		// $currSat = $row['Saturday'];
+	// 		// $currSun = $row['Sunday'];
 
 			
-		if(!empty($_POST['monday']))
-			$monday = $_POST['monday'];
-		else
-			$monday = $currMon;
+	// 	if(!empty($_POST['monday']))
+	// 		$monday = $_POST['monday'];
+	// 	else
+	// 		$monday = $currMon;
 
-		if(!empty($_POST['tuesday']))
-			$tuesday = $_POST['tuesday'];
-		else
-			$tuesday = $currTue;
+	// 	if(!empty($_POST['tuesday']))
+	// 		$tuesday = $_POST['tuesday'];
+	// 	else
+	// 		$tuesday = $currTue;
 
-		if(!empty($_POST['wednesday']))
-			$wednesday = $_POST['wednesday'];
-		else
-			$wednesday = $currWed;
+	// 	if(!empty($_POST['wednesday']))
+	// 		$wednesday = $_POST['wednesday'];
+	// 	else
+	// 		$wednesday = $currWed;
 		
-		if(!empty($_POST['thursday']))
-			$thursday = $_POST['thursday'];
-		else
-			$thursday = $currThur;
+	// 	if(!empty($_POST['thursday']))
+	// 		$thursday = $_POST['thursday'];
+	// 	else
+	// 		$thursday = $currThur;
 
-		if(!empty($_POST['friday']))
-			$friday = $_POST['friday'];
-		else
-			$friday = $currFri;
+	// 	if(!empty($_POST['friday']))
+	// 		$friday = $_POST['friday'];
+	// 	else
+	// 		$friday = $currFri;
 
-		if(!empty($_POST['saturday']))
-			$saturday = $_POST['saturday'];
-		else
-			$saturday = $currSat;
+	// 	if(!empty($_POST['saturday']))
+	// 		$saturday = $_POST['saturday'];
+	// 	else
+	// 		$saturday = $currSat;
 
-		if(!empty($_POST['sunday']))
-			$sunday = $_POST['sunday'];
-		else
-			$sunday = $currSun;
+	// 	if(!empty($_POST['sunday']))
+	// 		$sunday = $_POST['sunday'];
+	// 	else
+	// 		$sunday = $currSun;
 
-		$query = "UPDATE Users SET Monday='$monday', Tuesday='$tuesday', Wednesday='$wednesday', Thursday='$thursday', Friday='$friday', Saturday='$saturday', Sunday='$sunday' WHERE Username='$username'";
-		mysqli_query($conn, $query);
+	// 	$query = "UPDATE Users SET Monday='$monday', Tuesday='$tuesday', Wednesday='$wednesday', Thursday='$thursday', Friday='$friday', Saturday='$saturday', Sunday='$sunday' WHERE Username='$username'";
+	// 	mysqli_query($conn, $query);
 
-		// echo '<script type="text/javascript"> alert("Schedule Updated") </script>';
-		// // header() changes the page to the location listed
-		// header("Location: userMain.php");
-	}
+	// 	// echo '<script type="text/javascript"> alert("Schedule Updated") </script>';
+	// 	// // header() changes the page to the location listed
+	// 	// header("Location: userMain.php");
+	// }
 	
 ?>
 <!DOCTYPE html>
@@ -81,7 +116,7 @@
 		margin-right: auto;
 		font-family: arial, sans-serif;
 		border-collapse: collapse;
-		height: 200px;
+		/*height: 200px;*/
 		width: 25%;
 	}
 	a{
@@ -97,7 +132,7 @@
 	}
 	td, th {
 	  border: 1px solid #dddddd;
-	  text-align: left;
+	  text-align: center;
 	  padding: 8px;
 	  width:500px;
 	}
@@ -258,36 +293,75 @@
 <h1><u>Update Your Weekly Work Schedule</u></h1>
 </br>
 
+<!-- Displays the shift times which are generated from the shiftTimes table -->
 <h3>Shift Information</h3>
-
-</br>
-<table>
+<table style="margin-top: 6px; margin-bottom: 10px;">
 	<tr>
 		<th>Shifts</th>
 		<th>In</th>
 		<th>Out</th>
 	</tr>
-	<tr>
-		<td> Shift 1
-		<td> Time in
-		<td> Time out
-	</tr>
-	<tr>
-		<td> Shift 2
-		<td> Time in
-		<td> Time out
-	</tr>
-	<tr>
-		<td> Shift 3
-		<td> Time in
-		<td> Time out
-	</tr>
+	<?php 
+		$query = "SELECT * from ShiftTimes";
+		$result = mysqli_query($conn, $query);
+		$numOfRows = mysqli_num_rows($result);
+
+		$shiftNamesArr = array();
+		for($i = 0; $i<$numOfRows; $i++) {
+			$row = mysqli_fetch_assoc($result);
+
+			$shiftName = $row['ShiftName'];
+			array_push($shiftNamesArr, $shiftName);
+
+			$startTime = $row['StartTime'];
+			$endTime = $row['EndTime'];
+
+			echo "<tr>";
+			echo "<td>$shiftName</td>";
+			echo "<td>$startTime</td>";
+			echo "<td>$endTime</td>";
+			echo "</tr>";
+
+		}
+	 ?>
 </table>
-</br></br></br></br></br></br></br>
-<h3>Choose your personal availability</h3>
-</br>
-<table class="table1">
+
+<!-- Displays the availabiblity already entered by a user -->
+<h3>Current Availability</h3>
+<table style="margin-top: 10px; margin-bottom: 10px;">
 	<tr>
+		<th>Day</th>
+		<th>Availability</th>
+	</tr>
+	<?php 
+		$query = "SELECT * from Users where Username='$userCookie'";
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_assoc($result);
+		$userPin = $row['Pin'];
+		$_SESSION['Pin'] = $userPin;
+
+		$query = "SELECT * from Availability where Pin='$userPin'";
+		$result = mysqli_query($conn, $query);
+		$numOfRows = mysqli_num_rows($result);
+
+		for($i = 0; $i<$numOfRows; $i++) {
+			$row = mysqli_fetch_assoc($result);
+
+			$day = $row['Day'];
+			$shiftName = $row['ShiftName'];
+
+			echo "<tr>";
+			echo "<td>$day</td>";
+			echo "<td>$shiftName</td>";
+			echo "</tr>";
+		}
+	 ?>
+</table>
+
+<!-- Lets a User update their availability -->
+<h3>Choose your personal availability</h3>
+<!-- <table class="table1"> -->
+<!-- 	<tr>
 		<th><u>Monday</u></th>
 		<th><u>Tuesday</u></th>
 		<th><u>Wednesday</u></th>
@@ -332,13 +406,62 @@
 			 <input type="checkbox" name="Sun-3" value="check-1" id="check-1" > Third Shift</br>
 			 <input type="checkbox" name="Sun-0" value="check-1" id="check-1" > OFF
 		</td>
-	</tr>
-</table>
-		<div class="container">
+	</tr> -->
+<form method='post' action='userUpdateAvailabilty.php'>
+	<table style="margin-top: 10px; padding: 2px;">
+		<tr>
+			<th>Select Day</th>
+			<th>Select an Availability</th>
+		</tr>
+
+		<tr>
+			<td>
+				<select name="DaySelection">
+					<option selected disabled>Day Selection</option>
+					<option value="Monday">Monday</option>
+					<option value="Tuesday">Tuesday</option>
+					<option value="Wednesday">Wednesday</option>
+					<option value="Thursday">Thursday</option>
+					<option value="Friday">Friday</option>
+					<option value="Saturday">Saturday</option>
+					<option value="Sunday">Sunday</option>
+				</select>
+			</td>
+			<td>
+				<select name="ShiftNamesSelection">
+					<option selected disabled>Shift Selection</option>
+
+					<?php
+					$query = "SELECT * FROM ShiftTimes";
+					$result = mysqli_query($conn, $query);
+					$numOfRows = mysqli_num_rows($result);
+
+					for($i = 0; $i<$numOfRows; $i++) {
+						$row = mysqli_fetch_assoc($result);
+						$shiftName = $row['ShiftName'];
+						echo "<option value='$shiftName'>$shiftName</option>";
+					}
+					?>
+
+				</select>
+			</td>
+		</tr>
+	</table>
+	<input type="Submit" name="Add" value="Add to Availability"></input>
+</form>
+
+<form method="post" action="userMain.php">
+			<div style="text-align: center; padding: 2px;">
+				<!-- <input type="Submit" name="Submit" value="Back" style="background-color: #343131;  color: #969595;"></input> -->
+				<input type="Submit" name="Back" value="Back"></input>
+			</div>
+</form>
+
+		<!-- <div class="container">
 			<div class="popup" onclick="myFunction()">UPDATE
 				<span class="popuptext" id="myPopup">Updates sent to Manager!</span>
 			</div>
 			<a class="buttons" a href="/capstone-project-mohcamanagement/src/pages/userMain.php">CANCEL</a>
-		</div>
+		</div> -->
 </center>
 </html>
