e0df23f0f753a18124717322aa5af6dc32f5b6a8
dillonKilroy <dkilroy@emich.edu>
2021-03-28 20:04:28 -0400

    get the regex implemented for both the user creation pages.

--- src/pages/adminCreation.php
+++ src/pages/adminCreation.php
@@ -17,56 +17,75 @@
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["Create"])){
 		// checking to see if the username info was passed to the page from the form
 		if(!empty($_POST["Username"]) && !empty($_POST['Pin'])){
-			// if POST array has the variables in it then we save the variables 
+			// if POST array has the variables in it then we save the variables
 			$username = $_POST['Username'];
 			$pin = $_POST['Pin'];
 			setcookie("Username", $username);
-			if(!empty($_POST['Monday']))
-				$monday = $_POST['Monday'];
-			else
-				$monday = "Off";
 
-			if(!empty($_POST['Tuesday']))
-				$tuesday = $_POST['Tuesday'];
-			else
-				$tuesday = "Off";
+			// regex that checks to see if the username is a string and the pin is number
+			if(preg_match("/^[A-Z]{2}[a-z]+$/", $username) == 0 && preg_match("/^[0-9]{4}$/",$pin) == 0) {
+				echo "<script>alert('Error Username must be only letters. Pin must follow format \"xxxx\"')</script>";
+			}
 
-			if(!empty($_POST['Wednesday']))
-				$wednesday = $_POST['Wednesday'];
-			else
-				$wednesday = "Off";
+			elseif(preg_match("/^[A-Z]{2}[a-z]+$/", $username) == 0) {
+				echo "<script>alert('Error Username must be only letters.')</script>";
+			}
 
-			if(!empty($_POST['Thursday']))
-				$thursday = $_POST['Thursday'];
-			else
-				$thursday = "Off";
+			elseif(preg_match("/^[0-9]{4}$/",$pin) == 0) {
+				echo "<script>alert('Error pin must follow format \"xxxx\"')</script>";
+			}
 
-			if(!empty($_POST['Friday']))
-				$friday = $_POST['Friday'];
-			else
-				$friday = "Off";
+			else {
+				if(!empty($_POST['Monday']))
+					$monday = $_POST['Monday'];
+				else
+					$monday = "Off";
 
-			if(!empty($_POST['Saturday']))
-				$saturday = $_POST['Saturday'];
-			else
-				$saturday = "Off";
+				if(!empty($_POST['Tuesday']))
+					$tuesday = $_POST['Tuesday'];
+				else
+					$tuesday = "Off";
 
-			if(!empty($_POST['Sunday']))
-				$sunday = $_POST['Sunday'];
-			else
-				$sunday = "Off";
+				if(!empty($_POST['Wednesday']))
+					$wednesday = $_POST['Wednesday'];
+				else
+					$wednesday = "Off";
 
-			if(!empty($_POST['YearsWorked']))
-				$yearsWorked = $_POST['YearsWorked'];
-			else
-				$yearsWorked = 0;
+				if(!empty($_POST['Thursday']))
+					$thursday = $_POST['Thursday'];
+				else
+					$thursday = "Off";
 
-			// Use the saved variables to insert into the Users table
-			$query = "INSERT INTO Users VALUES($pin, '$username', 1, '$monday', '$tuesday', '$wednesday', '$thursday', '$friday', '$saturday', '$sunday', $yearsWorked)";
-			mysqli_query($conn, $query);
+				if(!empty($_POST['Friday']))
+					$friday = $_POST['Friday'];
+				else
+					$friday = "Off";
 
-			// header() changes the page to the location listed
-			header("Location: adminMain.php");
+				if(!empty($_POST['Saturday']))
+					$saturday = $_POST['Saturday'];
+				else
+					$saturday = "Off";
+
+				if(!empty($_POST['Sunday']))
+					$sunday = $_POST['Sunday'];
+				else
+					$sunday = "Off";
+
+				if(!empty($_POST['YearsWorked']))
+					$yearsWorked = $_POST['YearsWorked'];
+					if(preg_match("/^[0-9]{2}$/", $yearsWorked) == 0) {
+						echo "<script>alert('Error years worked need to be a number.</script>";
+					}
+				else
+					$yearsWorked = 0;
+
+				// Use the saved variables to insert into the Users table
+				$query = "INSERT INTO Users VALUES($pin, '$username', 1, '$monday', '$tuesday', '$wednesday', '$thursday', '$friday', '$saturday', '$sunday', $yearsWorked)";
+				mysqli_query($conn, $query);
+
+				// header() changes the page to the location listed
+				header("Location: adminMain.php");
+			}
 		}
 		else{
 			echo "<script id='requireEntries'>alert('Error Username and Pin must be entered.')</script>";
