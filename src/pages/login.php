f512de8f6127d8338b04bc0fe5b263b7c9987b63
jpoeana1 <jpoeana1@emich.edu>
2021-04-05 16:57:42 -0400

    Changed input values in companyRegister, and hopefully fixed the spacing for footer in about page.

--- src/pages/login.php
+++ src/pages/login.php
@@ -7,10 +7,10 @@
 		<title>Login Page</title>
 		<link rel="stylesheet" href="../style/style2.css?<?php echo time(); ?>">
 		<link rel="stylesheet" href="../style/style.css?<?php echo time(); ?>">
-		
 	</head>
 
-	<body class="login">	
+	<body class="login">
+
 	<form style="margin-top: 5%;" method="post" action="userCheck.php">
 		<div class="imgcontainer">
     		<img src="../images/avatar.png" alt="Avatar" class="avatar">
@@ -31,49 +31,12 @@
 			<span class="psw">New <a href="companyRegister.php">company?</a></span>
   		</div>
 	</form>
+
 	<form class="back" method="post" action="../index.html">
 		  <div class="container" style="background-color:#f1f1f1">
 			  <Button type="Submit" name="Submit" value="Back" class="backbtn" style="background-color: #343131;  color: #969595;">Back</Button>
 		  </div>
 	</form>
-
-		<!-- <table class="userCreationTable">
-			<form method="post" action="userCheck.php">
-				<tr>
-					<td style="padding: 2px;">
-						<input type="text" name="Username" placeholder="Enter Username" class="inputBox"/>
-					</td>
-
-					<td class="required">
-						<span>*</span>
-					</td>
-				</tr>
-
-				<tr>
-					<td style="padding: 2px;">
-						<input type="text" name="Pin" placeholder="Enter Pin" class="inputBox"/>
-					</td>
-
-					<td class="required">
-						<span>*</span>
-					</td>
-				</tr>
-
-				<tr>
-					<td style="text-align: center; padding: 2px;">
-						<Button type="Submit" name="Submit" style="background-color: #343131;  color: #969595;">Login</Button>
-					</td>
-				</tr>
-			</form> -->
-
-			<!-- <form method="post" action="../index.html">
-				<tr>
-					<td style="text-align: center; padding: 2px;">
-						<Button type="Submit" name="Submit" value="Back" style="background-color: #343131;  color: #969595;">Back</Button>
-					</td>
-				</tr>
-			</form>
-		</table> -->
 	
 	</body>
 </html>
