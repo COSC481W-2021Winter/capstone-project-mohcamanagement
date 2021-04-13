cc3d7b52d278e1ba7d4b190fb0561ad96dc633fc
JKuiper7 <jkuiper@emich.edu>
2021-04-12 20:04:39 -0400

    Finished the current schedule and work schedule implementation on the userMain page

--- src/pages/userMain.php
+++ src/pages/userMain.php
@@ -25,6 +25,7 @@
 	<title>User main page</title>
 	<link rel="stylesheet" href="../style/style1.css?<?php echo time(); ?>">
 	<link rel="stylesheet" href="../style/style6.css?<?php echo time(); ?>">
+	<link rel="stylesheet" href="../style/style7.css?<?php echo time(); ?>">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
  </head>
 
@@ -42,8 +43,75 @@
 	<body>
 		<?php 
 		echo "<h1>Welcome $userCookie</h1>";
+		$query = "SELECT * FROM CurrentSchedule WHERE Username='$userCookie'";
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_assoc($result);
+		
+		$user = $row['Username'];
+		$monday = $row['Monday'];
+		$tuesday = $row['Tuesday'];
+		$wednesday = $row['Wednesday'];
+		$thursday = $row['Thursday'];
+		$friday = $row['Friday'];
+		$saturday = $row['Saturday'];
+		$sunday = $row['Sunday'];
 		 ?>
-		<div class="square1">Schedule Placeholder</div>
-		<div class="square2">Suggested Inventory / Writeoffs Placeholder</div>
+		 
+		<h3>Shift Information</h3>
+		<div class="inside-table">
+			<table style="margin-top: 6px; margin-bottom: 10px;">
+				<tr>
+					<th>Shifts</th>
+					<th>In</th>
+					<th>Out</th>
+				</tr>
+				<?php 
+					$query = "SELECT * from ShiftTimes";
+					$result = mysqli_query($conn, $query);
+					$numOfRows = mysqli_num_rows($result);
+
+					for($i = 0; $i<$numOfRows; $i++) {
+						$row = mysqli_fetch_assoc($result);
+
+						$shiftName = $row['ShiftName'];
+
+						$startTime = $row['StartTime'];
+						$endTime = $row['EndTime'];
+
+						echo "<tr>";
+						echo "<td>$shiftName</td>";
+						echo "<td>$startTime</td>";
+						echo "<td>$endTime</td>";
+						echo "</tr>";
+
+					}
+				 ?>
+			</table>
+			
+			<h3>Your Work Week</h3>
+			<table>
+				<tr>
+					<th>Monday</th>
+					<th>Tuesday</th>
+					<th>Wednesday</th>
+					<th>Thursday</th>
+					<th>Friday</th>
+					<th>Saturday</th>
+					<th>Sunday</th>
+				</tr>
+				<tr>
+				<?php
+					echo "<td>$monday</td>";
+					echo "<td>$tuesday</td>";
+					echo "<td>$wednesday</td>";
+					echo "<td>$thursday</td>";
+					echo "<td>$friday</td>";
+					echo "<td>$saturday</td>";
+					echo "<td>$sunday</td>";
+				?>
+				</tr>
+			</table>
+			<h3>Suggested Inventory / Writeoffs Placeholder</h3>
+		</div>
 	</body>
 </html>
